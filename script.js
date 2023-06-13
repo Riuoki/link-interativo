2339ff19f5369dedcdbf1ae5f1c02b5ff616d2a1
Riuoki <emaildojoaomauro@gmail.com>
2023-06-13 17:19:34 -0300

    bug fixes

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ html.classList.toggle ("light")
 
 const img = document.querySelector('#profile img')
 if (html.classList.contains('light')){
-    img.setAttribute('src', '../assets/avatar-light.png')
+    img.setAttribute('src', './assets/avatar-light.png')
 
 } 
 else{
-    img.setAttribute('src', '../assets/avatar-dark.png')
+    img.setAttribute('src', './assets/avatar-dark.png')
 }
 
 }
